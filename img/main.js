9fb5729ddd719f9e0ea1e1bfc10354be17a90d6b
duke3c <stopthebruh@gmail.com>
2022-06-01 17:51:52 +0000

    got it working the discord bot with a prefix and two commands this is the beginning

---
+++ img/main.js
@@ -0,0 +1,23 @@
+const { Client, Intents } = require("discord.js");
+require("dotenv").config()
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+console.log()
+// Set the prefix
+let prefix = "/";
+client.on("messageCreate", (message) => {
+  // Exit and stop if the prefix is not there or if user is a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(`${prefix}ping`)) {
+    message.channel.send("pong!");
+  } else
+
+  if (message.content.startsWith(`${prefix}foo`)) {
+    message.channel.send("bar!");
+  }
+});
+
+client.login(process.env.TOKEN);
